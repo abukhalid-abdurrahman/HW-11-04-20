29a62d996a89103deb12e71a159dc98b5ef3b892
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-12 16:43:29 +0300

    Финальная версия задачи

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ namespace Day_11
         {
             TableManager tM = new TableManager("dbo.students");
             tM.AddColumns("ID", "Name", "Surname", "PhoneNumber", "HomeAdress");
-            tM.AddRow("1", "Фаридун", "Бердиев", "+992901915905", "Дехоти 45/3");
+            tM.AddRow("1", "Фаридун", "Бердиев", "+992906908008", "Дехоти 45/3");
+            tM.AddRow("2", "Мичгона", "Чумаева", "+992988000111", "Дехоти 42/1");
+            tM.AddRow("3", "Андрей", "Васильевич", "+992778877777", "Дехоти 48/1");
+            tM.AddRow("4", "Рухсора", "Рухсоровна", "+992666888999", "Сино 15/9");
+            tM.Output();
         }
     }
 }
